00dabecac2285a22d3883c0a7cdfa717d93e8e6a
X230 <343434632@qq.com>
2019-07-08 14:59:05 +0800

    lxchun 添加配置文件配置公共参数

--- network/tests.py
+++ network/tests.py
@@ -1,5 +1,25 @@
 from django.test import TestCase
+import ConfigParser
 
 # Create your tests here.
 
 print("123")
+
+
+# 读取配置文件样例
+def test_reader():
+    """
+    读取配置文件样例
+    :return:
+    """
+    cp = ConfigParser.SafeConfigParser()
+    cp.read('myapp.conf')
+    print 'options of [file]:', cp.options('file')
+    print 'fileUrl of file:', cp.get('file', 'fileUrl')
+
+    print 'options of [esAddress]:', cp.options('esAddress')
+    print 'port of esAddress:', cp.get('esAddress', 'port')
+    print 'ip of esAddress:', cp.get('esAddress', 'ip')
+
+
+test_reader();
